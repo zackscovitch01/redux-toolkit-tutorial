0225390c838b2971131d63a1969491ef1522621c
zackscovitch01 <bagoudoumoubarack@icloud.com>
2024-06-22 11:39:32 -0400

    removeItem action

--- src/components/CartContainer.jsx
+++ src/components/CartContainer.jsx
@@ -1,7 +1,10 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import CartItem from "./CartItem";
+import { clearCart } from "../features/cart/cartSlice";
 
 const CartContainer = () => {
+  const dispatch = useDispatch();
+
   const { cartItems, total, amount } = useSelector((store) => store.cart);
 
   if (amount < 1) {
@@ -31,7 +34,9 @@ const CartContainer = () => {
             total <span>${total}</span>
           </h4>
         </div>
-        <button className="btn clear-btn">clear cart</button>
+        <button className="btn clear-btn" onClick={() => dispatch(clearCart())}>
+          clear cart
+        </button>
       </footer>
     </section>
   );
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -11,6 +11,17 @@ const initialState = {
 const cartSlice = createSlice({
   name: "cart",
   initialState: initialState,
+  reducers: {
+    clearCart: (state) => {
+      state.cartItems = [];
+    },
+    removeItem: (state, action) => {
+      const itemId = action.payload;
+      state.cartItems = state.cartItems.filter((item) => item.id !== itemId);
+    },
+  },
 });
+
+export const { clearCart, removeItem } = cartSlice.actions;
 
 export default cartSlice.reducer;
